9417e9c0945d91985610e3e7b9c46c0f4d5715ed
William Panting <willtp87@hotmail.com>
2011-10-19 14:04:58 -0300

    Checking for CModel, Collection.  getting more mods relations

--- movie_ingest.py
+++ movie_ingest.py
@@ -16,7 +16,7 @@ etree = xmlib.import_etree()
 Helper functions
 '''
 
-def handle_clip_mods(clip_mods_parser, mods_file_path):
+def handle_clip_mods(clip_mods_parser, mods_file_name):
     '''
     This function will handle the creation of clip objects
     @param clip_mods_parser
@@ -34,20 +34,21 @@ def handle_clip_mods(clip_mods_parser, mods_file_path):
     
     clip_pid = fedora.getNextPID(name_space)
     
-    high_resolution_mov_path = os.path.join(os.path.dirname(mods_file_path), high_resolution_mov_path)
-    low_resolution_mov_path = os.path.join(os.path.dirname(mods_file_path), low_resolution_mov_path)
+    high_resolution_mov_path = os.path.normpath(os.path.join(mods_directory, high_resolution_mov_path))
+    low_resolution_mov_path = os.path.normpath(os.path.join(mods_directory, low_resolution_mov_path))
+    
     
     #this section handles the diferent types of clips (subs or not)
-    if not '-sub' in os.path.basename(mods_file_path):
+    if not '-sub' in mods_file_name:
         global clips_to_pids
-        clips_to_pids[mods_file] = clip_pid
+        clips_to_pids[mods_file_name] = clip_pid
         print('clip')
     else:
         print('clip with subs')
         
     return True
     
-def handle_misc_mods(misc_mods_parser, mods_file):
+def handle_misc_mods(misc_mods_parser, mods_file_name):
     '''
     This function will handle the creation of clip objects
     @param misc_mods_parser
@@ -55,11 +56,17 @@ def handle_misc_mods(misc_mods_parser, mods_file):
     @return boolean
       True on success, false if something was wrong
     '''
-    
+    '''
+    misc_element_list = misc_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'location']//*[local-name() = 'url'][@displayLabel='Web Quality Video']")
+    if miscelement_list:
+        misc_path = misc_element_list[0].text
+        misc_path = os.path.normpath(os.path.join(mods_directory, misc_path))
+        print(misc_path)
+    '''
     print('misc')
     return True
 
-def handle_still_mods(still_mods_parser, mods_file):
+def handle_still_mods(still_mods_parser, mods_file_name):
     '''
     This function will handle the creation of clip objects
     @param still_mods_parser
@@ -68,10 +75,16 @@ def handle_still_mods(still_mods_parser, mods_file):
       True on success, false if something was wrong
     '''
     
-    print('still')
-    return True
+    still_element_list = still_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'location']//*[local-name() = 'url'][@displayLabel='Still Image']")
+    if still_element_list:
+        still_path = still_element_list[0].text
+        still_path = os.path.normpath(os.path.join(mods_directory, still_path))
+        print(still_path)
+        print('still')
+        return True
+    return False
 
-def handle_transcript_mods(transcript_mods_parser, mods_file):
+def handle_transcript_mods(transcript_mods_parser, mods_file_name):
     '''
     This function will handle the creation of clip objects
     @param transcript_mods_parser
@@ -80,8 +93,14 @@ def handle_transcript_mods(transcript_mods_parser, mods_file):
       True on success, false if something was wrong
     '''
     
-    print('transcript')
-    return True
+    transcript_element_list = transcript_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'location']//*[local-name() = 'url'][@displayLabel='Document']")
+    if transcript_element_list:
+        transcript_path = transcript_element_list[0].text
+        transcript_path = os.path.normpath(os.path.join(mods_directory, transcript_path))
+        print(transcript_path)
+        print('transcript')
+        return True
+    return False
 
     
 def handle_mods_file(mods_file_path):
@@ -96,17 +115,19 @@ def handle_mods_file(mods_file_path):
     @author William Panting
     '''
     mods_tree = etree.parse(mods_file_path)
+    mods_file_name = os.path.basename(mods_file_path)
+    
     if '-cp' in mods_file_path:
-        if handle_clip_mods(mods_tree, mods_file_path) is False:
+        if handle_clip_mods(mods_tree, mods_file_name) is False:
             return False
     if '-mis' in mods_file_path:
-        if handle_misc_mods(mods_tree, mods_file_path) is False:
+        if handle_misc_mods(mods_tree, mods_file_name) is False:
             return False
     if '-st' in mods_file_path:
-        if handle_still_mods(mods_tree, mods_file_path) is False:
+        if handle_still_mods(mods_tree, mods_file_name) is False:
             return False
     if '-tr-' in mods_file_path:
-        if handle_transcript_mods(mods_tree, mods_file_path) is False:
+        if handle_transcript_mods(mods_tree, mods_file_name) is False:
             return False    
     return True
 
@@ -163,26 +184,49 @@ if __name__ == '__main__':
         sys.exit()
         
     #prep data structures (files)
-    mods_files = os.listdir(mods_directory)
+    mods_file_names = os.listdir(mods_directory)
     data_files = os.listdir(data_directory)
     
-    #restrict mods_files to .xml
-    mods_files_copy = list(mods_files)
-    for mods_file in mods_files_copy:
-        if not mods_file.endswith('.xml'):
-            mods_files.remove(mods_file)
+    #restrict mods_file_names to .xml
+    mods_file_names_copy = list(mods_file_names)
+    for mods_file_name in mods_file_names_copy:
+        if not mods_file_name.endswith('.xml'):
+            mods_file_names.remove(mods_file_name)
     
     #this mapping is to link pids to clip files to make building relationships easier
-    clips_to_pids = dict({'1':'1', '1':'1'})
+    clips_to_pids = dict()
     
     '''
     do ingest
     '''
     #create a movie and benshi object because parent pids must be known
     #@todo: change the labels to be more meaningful later in the script
-    mods_file = mods_files[0]
+    mods_file_name = mods_file_names[0]
     
-    movie_name = mods_file[:mods_file.find('-')]
+    #put in the JapaneseSilentFilmCollection collection object
+    try:
+        collection_label = u'JapaneseSilentFilmCollection'
+        collection_pid = unicode(name_space + ':' + collection_label)
+        fedora.getObject(collection_pid)
+    except FedoraConnectionException, object_fetch_exception:
+        if object_fetch_exception.httpcode in [404]:
+            logging.info(name_space + ':JapaneseSilentFilmCollection missing, creating object.\n')
+            '''
+            collection_object = fedora.createObject(collection_pid, label = collection_label)
+            '''
+    #put in the benshi Islandora:BenshiMovie content model
+    try:
+        model_pid = u'islandora:BenshiMovie'
+        fedora.getObject(model_pid)
+    except FedoraConnectionException, object_fetch_exception:
+        if object_fetch_exception.httpcode in [404]:
+            logging.info('islandora:BenshiMovie missing, creating object.\n')
+            '''
+            model_object = fedora.createObject(model_pid, label = u'BenshiMovieCModel')
+            '''
+    
+    
+    movie_name = mods_file_name[:mods_file_name.find('-')]
     movie_pid = fedora.getNextPID(name_space)
     benshi_pid = fedora.getNextPID(name_space)
     
@@ -192,27 +236,27 @@ if __name__ == '__main__':
     benshi_label=unicode(movie_name + ' Benshi')
     benshi_object = fedora.createObject(benshi_pid, label = benshi_label)
     
-    print('starter objects')
+    print('starter objects ingested')
     
     #loop through the mods folder and ingest the clips because parent pids mst be known
-    mods_files_copy = list(mods_files)
-    for mods_file in mods_files_copy:
+    mods_file_names_copy = list(mods_file_names)
+    for mods_file_name in mods_file_names_copy:
         #proceed if mods file is for a clip but not one with subs
-        if '-cp' in mods_file and not '-sub' in mods_file:
-            mods_file_full_path = os.path.join(mods_directory, mods_file)
+        if '-cp' in mods_file_name and not '-sub' in mods_file_name:
+            mods_file_path = os.path.join(mods_directory, mods_file_name)
             #ingest
-            if handle_mods_file(mods_file_full_path) is False:
-                logging.error('Error handling mods_file: ' + mods_file_full_path + ' \n')
+            if handle_mods_file(mods_file_path) is False:
+                logging.error('Error handling mods_file_name: ' + mods_file_path + ' \n')
                 sys.exit()
-            #remove the file from mods_files list
-            mods_files.remove(mods_file)
-    print('clips')
+            #remove the file from mods_file_names list
+            mods_file_names.remove(mods_file_name)
+    print('Main Clips ingested')
     
     #loop through the rest of the files and ingest the data
-    for mods_file in mods_files:
-        mods_file_full_path = os.path.join(mods_directory, mods_file)
-        if handle_mods_file(mods_file_full_path) is False:
-            logging.error('Error handling mods_file: ' + mods_file_full_path + ' \n')
+    for mods_file_name in mods_file_names:
+        mods_file_path = os.path.join(mods_directory, mods_file_name)
+        if handle_mods_file(mods_file_path) is False:
+            logging.error('Error handling mods_file_name: ' + mods_file_path + ' \n')
             sys.exit()
         
 sys.exit()
