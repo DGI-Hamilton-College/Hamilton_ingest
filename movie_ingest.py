a0ff78f6e1f1c4915217ca18362e1c42ccf606be
William Panting <willtp87@hotmail.com>
2011-10-21 09:18:02 -0300

    bug fixes, better namespaceing on relsext, better coding

--- movie_ingest.py
+++ movie_ingest.py
@@ -8,9 +8,7 @@ import logging, sys, os, ConfigParser, time#, shutil
 from fcrepo.connection import Connection, FedoraConnectionException
 from fcrepo.client import FedoraClient
 from islandoraUtils.metadata import fedora_relationships
-from islandoraUtils import xmlib
-etree = xmlib.import_etree()
-
+from lxml import etree
 
 '''
 Helper functions
@@ -41,22 +39,22 @@ def handle_clip_mods(clip_mods_parser, mods_file_name):
     
     clip_label=unicode(movie_name + '_' + mods_file_name[mods_file_name.find('-')+1:mods_file_name.rfind('.')])
     clip_object = fedora.createObject(clip_pid, label = clip_label)
-    clip_object_RELS_EXT=fedora_relationships.rels_ext(clip_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
+    clip_object_RELS_EXT=fedora_relationships.rels_ext(clip_object,[hamilton_rdf_name_space, fedora_model_namespace])
 
+    global clips_to_pids
+    clips_to_pids[mods_file_name] = clip_pid
     
     #this section handles the diferent types of clips (subs or not)
     if not '-sub' in mods_file_name:
-        global clips_to_pids
-        clips_to_pids[mods_file_name] = clip_pid
             #add relationships
-        clip_object_RELS_EXT.addRelationship('isClipOf', movie_object)
+        clip_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isClipOf'), movie_object)
         clip_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':benshiClip')
         clip_object_RELS_EXT.update()
         return True
     else:
         #add relationships
         master_clip_file_name = mods_file_name.replace('-sub','')
-        clip_object_RELS_EXT.addRelationship('isSubOf', clips_to_pids[master_clip_file_name])
+        clip_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isSubOf'), clips_to_pids[master_clip_file_name])
         clip_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':benshiClipSubbed')
         clip_object_RELS_EXT.update()
         return True
@@ -70,47 +68,49 @@ def handle_misc_mods(misc_mods_parser, mods_file_name):
     @return boolean
       True on success, false if something was wrong
     '''
-    misc_type_list = misc_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'genre][@type='local']")
+    misc_type_list = misc_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'genre'][@type='local']")
     if misc_type_list:
         misc_type = misc_type_list[0].text
         print(misc_type)
-        if misc_type is 'sound recording':#fix up benshi object
+        if misc_type == 'sound recording':#fix up benshi object
             print('benshi')
             
-        elif misc_type is 'essay':
+        elif misc_type == 'essay':
             misc_pid = fedora.getNextPID(name_space)
             misc_label = unicode(movie_name + '_' + misc_type)
             misc_object = fedora.createObject(misc_pid, label = misc_label)
-            misc_object_RELS_EXT = fedora_relationships.rels_ext(misc_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
+            misc_object_RELS_EXT = fedora_relationships.rels_ext(misc_object,[hamilton_rdf_name_space, fedora_model_namespace])
         
-            misc_object_RELS_EXT.addRelationship('isEssayOf', movie_pid)
+            misc_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isEssayOf'), movie_pid)
             misc_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':benshiEssay')
-
-        elif misc_type is 'presentation':
+            misc_object_RELS_EXT.update()
+            
+        elif misc_type == 'presentation':
             misc_pid = fedora.getNextPID(name_space)
             misc_label = unicode(movie_name + '_' + misc_type)
             misc_object = fedora.createObject(misc_pid, label = misc_label)
-            misc_object_RELS_EXT = fedora_relationships.rels_ext(misc_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
+            misc_object_RELS_EXT = fedora_relationships.rels_ext(misc_object,[hamilton_rdf_name_space, fedora_model_namespace])
         
-            misc_object_RELS_EXT.addRelationship('isPresentationOf', movie_pid)
+            misc_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isPresentationOf'), movie_pid)
             misc_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':benshiPresentation')
-
-        elif misc_type is 'Motion Picture':#fix up movie object
+            misc_object_RELS_EXT.update()
+            
+        elif misc_type == 'Motion Picture':#fix up movie object
             print('movie')
             
-        elif misc_type is 'biography':
+        elif misc_type == 'biography':
             misc_pid = fedora.getNextPID(name_space)
             misc_label = unicode(movie_name + '_Narrator')
             misc_object = fedora.createObject(misc_pid, label = misc_label)
-            misc_object_RELS_EXT = fedora_relationships.rels_ext(misc_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
+            misc_object_RELS_EXT = fedora_relationships.rels_ext(misc_object,[hamilton_rdf_name_space, fedora_model_namespace])
         
-            misc_object_RELS_EXT.addRelationship('isNarratorOf', benshi_pid)
+            misc_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isNarratorOf'), benshi_pid)
             misc_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':benshiNarrator')
+            misc_object_RELS_EXT.update()
 
         else:
             return False
         
-        misc_object_RELS_EXT.update()
     return True
 
 def handle_still_mods(still_mods_parser, mods_file_name):
@@ -130,7 +130,7 @@ def handle_still_mods(still_mods_parser, mods_file_name):
         
         still_label = unicode(movie_name + '_' + mods_file_name[mods_file_name.find('-')+1:mods_file_name.rfind('.')])
         still_object = fedora.createObject(still_pid, label = still_label)
-        still_object_RELS_EXT = fedora_relationships.rels_ext(still_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
+        still_object_RELS_EXT = fedora_relationships.rels_ext(still_object,[hamilton_rdf_name_space, fedora_model_namespace])
         
         
         
@@ -138,9 +138,9 @@ def handle_still_mods(still_mods_parser, mods_file_name):
         still_clip_element_list = still_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'location']//*[local-name() = 'url'][@displayLabel='Video clip']")
         if still_clip_element_list:
             still_clip_file_name = still_clip_element_list[0].text
-            still_object_RELS_EXT.addRelationship('isStillOf', clips_to_pids[still_clip_file_name])
+            still_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isStillOf'), clips_to_pids[still_clip_file_name])
         else:
-            still_object_RELS_EXT.addRelationship('isStillOf', movie_pid)
+            still_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isStillOf'), movie_pid)
         
         still_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':benshiStill')
         still_object_RELS_EXT.update()
@@ -157,22 +157,24 @@ def handle_transcript_mods(transcript_mods_parser, mods_file_name):
       True on success, false if something was wrong
     '''
     
+    transcript_pid = fedora.getNextPID(name_space)
+    transcript_label = unicode(movie_name + '_' + mods_file_name[mods_file_name.find('-tr-') + 4:mods_file_name.rfind('.')])
+    transcript_object = fedora.createObject(transcript_pid, label = transcript_label)
+    transcript_object_RELS_EXT = fedora_relationships.rels_ext(transcript_object,[hamilton_rdf_name_space, fedora_model_namespace])
+        
     transcript_element_list = transcript_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'location']//*[local-name() = 'url'][@displayLabel='Document']")
     if transcript_element_list:
         transcript_path = transcript_element_list[0].text
         transcript_path = os.path.normpath(os.path.join(mods_directory, transcript_path))
         
-        
-        transcript_pid = fedora.getNextPID()
-        transcript_label = unicode(movie_name + '_' + mods_file_name[mods_file_name.find('-tr-') + 4:mods_file_name.rfind('.')])
-        transcript_object = fedora.createObject(transcript_pid, label = transcript_label)
-        transcript_object_RELS_EXT = fedora_relationships.rels_ext(transcript_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
-        
-        transcript_clip_element_list = still_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'location']//*[local-name() = 'url'][@displayLabel='Video clip']")
+        #handle is transcript of
+        transcript_clip_element_list = transcript_mods_parser.xpath("//*[local-name() = 'mods']//*[local-name() = 'location']//*[local-name() = 'url'][@displayLabel='Video clip']")
         if transcript_clip_element_list:
             transcript_clip_file_name = transcript_clip_element_list[0].text
-            transcript_object_RELS_EXT.addRelationship('isTranscriptOf', clips_to_pids[transcript_clip_file_name])
-        
+            transcript_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isTranscriptOf'), clips_to_pids[transcript_clip_file_name])
+        else:
+            transcript_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton','isTranscriptOf'), movie_name)
+            
         #handle the 3 different transcript types
         if '-jpneng' in mods_file_name:
             transcript_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':EnglishJapaneseTranscript')
@@ -182,7 +184,7 @@ def handle_transcript_mods(transcript_mods_parser, mods_file_name):
             transcript_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':EnglishTranscript')
         else:
             return False
-        
+    
         transcript_object_RELS_EXT.update()
         
         return True
@@ -229,14 +231,17 @@ if __name__ == '__main__':
     '''
     setup
     '''
-    name_space = u'hamilton2'
+    name_space = u'hamilton4'
         
+    hamilton_rdf_name_space = fedora_relationships.rels_namespace('hamilton', 'http://hamilton.org/ontology#')
+    fedora_model_namespace = fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#')
+    
     #configure logging
-    log_directory=os.path.join(source_directory,'logs')
+    log_directory = os.path.join(source_directory,'logs')
     if not os.path.isdir(log_directory):
         os.mkdir(log_directory)
     logFile=os.path.join(log_directory,'Hamilton_batch_movie_ingest' + time.strftime('%y_%m_%d') + '.log')
-    logging.basicConfig(filename=logFile, level=logging.DEBUG)
+    logging.basicConfig(filename = logFile, level=logging.DEBUG)
 
     #get config
     config = ConfigParser.ConfigParser()
@@ -299,7 +304,7 @@ if __name__ == '__main__':
             logging.info(name_space + ':JapaneseSilentFilmCollection missing, creating object.\n')
             collection_object = fedora.createObject(collection_pid, label = collection_label)
             #add relationships
-            collection_object_RELS_EXT=fedora_relationships.rels_ext(collection_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
+            collection_object_RELS_EXT=fedora_relationships.rels_ext(collection_object,fedora_model_namespace)
             collection_object_RELS_EXT.addRelationship('isMemberOf','islandora:top')
             collection_object_RELS_EXT.update()
     #put in the benshi Islandora:BenshiMovie content model
@@ -311,7 +316,7 @@ if __name__ == '__main__':
             logging.info('islandora:BenshiMovie missing, creating object.\n')
             model_object = fedora.createObject(model_pid, label = u'BenshiMovieCModel')
             #add relationships
-            model_object_RELS_EXT=fedora_relationships.rels_ext(model_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
+            model_object_RELS_EXT=fedora_relationships.rels_ext(model_object,fedora_model_namespace)
             model_object_RELS_EXT.addRelationship('isMemberOf','islandora:top')
             model_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'),'islandora:collectionCModel')
             model_object_RELS_EXT.update()
@@ -324,7 +329,7 @@ if __name__ == '__main__':
     movie_label=unicode(movie_name)
     movie_object = fedora.createObject(movie_pid, label = movie_label)
     #add relationships
-    movie_object_RELS_EXT=fedora_relationships.rels_ext(movie_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
+    movie_object_RELS_EXT=fedora_relationships.rels_ext(movie_object,fedora_model_namespace)
     movie_object_RELS_EXT.addRelationship('isMemberOf',collection_pid)
     movie_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), model_pid)
     movie_object_RELS_EXT.update()
@@ -332,8 +337,8 @@ if __name__ == '__main__':
     benshi_label=unicode(movie_name + ' Benshi')
     benshi_object = fedora.createObject(benshi_pid, label = benshi_label)
     #add relationships
-    benshi_object_RELS_EXT=fedora_relationships.rels_ext(benshi_object,fedora_relationships.rels_namespace('fedora-model','info:fedora/fedora-system:def/model#'))
-    benshi_object_RELS_EXT.addRelationship('isBenshiOf',movie_object)
+    benshi_object_RELS_EXT=fedora_relationships.rels_ext(benshi_object,[hamilton_rdf_name_space, fedora_model_namespace])
+    benshi_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('hamilton', 'isBenshiOf'), movie_object)
     benshi_object_RELS_EXT.addRelationship(fedora_relationships.rels_predicate('fedora-model','hasModel'), name_space + ':benshiRecording')
     benshi_object_RELS_EXT.update()
     
